47cf273add5e36a728b20e2b3620d1ba78b3ae2b
vasu.arya97@gmail.com <vasu.arya97@gmail.com>
2023-12-09 15:43:09 +0530

    Second Header Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
+import { SecondHeader } from "./components/secondHeader/SecondHeader";
 import { GlobalHeader } from "./components/globalHeader/GlobalHeader";
 
-// 1. LogIn -
-// 2. Home Page -
-// 3. Sign Up -
-
 export default function App() {
-
   return (
     <div className="App">
       <GlobalHeader />
-      
       <br />
-      <div className="joybell">
-        <h5>Joyous Bells: Celebrating Christmas Cheer.</h5>
-        <button className="knowmorebutton">Know More</button>
-      </div>
+      <SecondHeader />
     </div>
   );
 }
+
 
---
+++ src/components/secondHeader/SecondHeader.js
@@ -0,0 +1,10 @@
+import "./secondHeader.css";
+
+export function SecondHeader() {
+  return (
+    <div className="joybell">
+      <h5>Joyous Bells: Celebrating Christmas Cheer.</h5>
+      <button className="knowmorebutton">Know More</button>
+    </div>
+  );
+}
